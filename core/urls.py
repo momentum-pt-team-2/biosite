b3d3ba499a9cd8939d968a1b82f5130836c20299
crodri00 <rodriguez.clarissa@live.com>
2021-08-24 21:05:00 -0400

    Working simple page

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from students import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('registration.backends.simple.urls')),
+    path('crodri/', views.clarissa_page, name='clarissa_page')
 ]
