3911c81dae53e733243e1f499c0d5a672ed29d2f
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2021-08-26 19:25:55 -0400

    Update models.py

--- students/models.py
+++ students/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    bio = models.TextField(blank=True, null=True)
-    name = models.CharField(max_length=200, blank=True, null=True)
+    bio = models.CharField(blank=True, null=True)
+    name = models.TexFieldField(max_length=200, blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
-    phone = models.CharField(blank=True, null=True, max_length=14)
+    phone = models.IntegerField(blank=True, null=True, max_length=14)
     skills = models.TextField(blank=True, null=True)
