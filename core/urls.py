63047ef146aa56b00bd1ddc4226427f987ade237
Marabrie <mararbrie@gmail.com>
2021-08-25 21:37:24 -0400

    update photo

--- core/urls.py
+++ core/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from students import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('registration.backends.simple.urls')),
+    path('marabrie/', views.maraspage, name="maraspage")
 ]
+
+
--- students/views.py
+++ students/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import User
 # Create your views here.
+
+def maraspage(request):
+    mara_user = User.objects.get(username='marabrie')
+    return render(request, 'students/mara_page.html', {'mara_user': mara_user})
+
+
