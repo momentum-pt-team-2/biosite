bd22dd4c5b4242554f83b8b8d3b50217e76d7336
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2021-08-26 19:22:59 -0400

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 
-def main():
+def rebecca_trying_to_break_it():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
     try:
